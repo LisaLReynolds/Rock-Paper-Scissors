f68bd749d3d22766266a911eb25c734008cf4084
LisaLReynolds <lisalreynolds2015@gmail.com>
2024-10-30 20:53:54 -0600

    added to script file

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello, World!");
+
+let choice = ["rock", "paper", "scissors"];
+
+//Write a function that randomly selects rock, paper, or scissors for the computer and a function that gets the user's choice. then compare the two choices and determine the winner.
