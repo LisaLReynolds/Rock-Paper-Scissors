6ac7ab41c55af0cf7687a96b759537657efe76c4
LisaLReynolds <lisalreynolds2015@gmail.com>
2025-02-07 16:49:29 -0700

    updates to js including userChoice

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 let choice = ["rock", "paper", "scissors"];
 
 //Write a function that randomly selects rock, paper, or scissors for the computer and a function that gets the user's choice. then compare the two choices and determine the winner.
+
+if (window.confirm("Do you really want to leave?")) {
+    window.open("exit.html", "Thanks for Visiting!");
+  }
